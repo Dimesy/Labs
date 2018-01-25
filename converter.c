bc2a4273932e7afc34aca84548b2efeb66ac961c
Dimesy <34558764+Dimesy@users.noreply.github.com>
2018-01-26 02:06:38 +0300

    Update converter.c

--- converter.c
+++ converter.c
@@ -1,76 +1,69 @@
 #include <stdio.h> 
-#include <string.h> 
 #include <stdlib.h> 
+#include<stdio.h>
 
-void change(double number, char des)
-{
-	if (des == 'C')
-	{
-		if (number < -273.15)
-		{
-			printf("Absolute zero for Celsius\n");
-			return;
-		}
-		printf("%.2f C\n", number);
-		printf("%.2f F\n", number * (9.0 / 5.0) + 32.0);
-		printf("%.2f K\n", number + 273.15);
-	}
-	if (des == 'F')
-	{
-		if (number < -459.00)
-		{
-			printf("Absolute zero for Fahrenheit\n");
-			return;
-		}
-		printf("%.2f F\n", number);
-		printf("%.2f C\n", (number - 32.0) * 5.0 / 9.0);
-		printf("%.2f K\n", (number - 32.0) * 5.0 / 9.0 + 273.15);
-	}
-	if (des == 'K')
-	{
-		if (number < 0.00)
-		{
-			printf("Absolute zero for Calvin\n");
-			return;
-		}
-		printf("%.2f K\n", number);
-		printf("%.2f C\n", number - 273.15);
-		printf("%.2f F\n", (number - 273.15) * (9.0 / 5.0) + 32.0);
-	}
+float C_F (float x){
+	return x*1.8+32;
+}
+float F_C (float x){
+	return (x-32)*5.0/9.0;
+}
+float C_K (float x){
+	return x+273.15;
+}
+float K_C (float x){
+	return x-273.15;
 }
 
-int main(int argc, char *argv[])
-{
-
-	double grad;
-	char design;
-
-	if (argc == 1 || argc>3) {
-		printf("Error! Enter arguments.");
-		return 0;
+int main(int argc, char *argv[]){
+	float c;
+	if (argc!=1)
+		c = atof(argv[1]);
+	else {
+		printf("Error!");
+		return 1;
 	}
+	if (argc==3)
+		switch (*argv[2]){
+			case 'C':
+			case 'c':
+				if (C_K(c) < 0)
+					printf("Temperature < absolute zero for Celsius!\n");
+				else
+					printf("%.2lf F\n%.2f K", C_F(c), C_K(c));
+				break;
+			case 'F':
+			case 'f':
+				if (C_K(F_C(c)) < 0)	
+					printf("Temperature < absolute zero for Fahrenheit!\n");
+				else	
+					printf("%.2f C\n%.2f K", F_C(c), C_K(F_C(c)));
+				break;
+			case 'K':
+			case 'k':
+				if (c < 0)
+					printf("Temperature < absolute zero for Kelvin!\n");
+				else
+					printf("%.2f C\n%.2f F", K_C(c), C_F(K_C(c)));
+				break;
+		}
+	else {
+		printf ("%.2f C:\n", c);
+		if (C_K(c) < 0)
+			printf("Temperature < absolute zero for Celsius!\n");
+		else
+			printf("%.2f F\n%.2f K\n", C_F(c), C_K(c));
+		printf ("%.2f F:\n", c);
+		if (C_K(F_C(c)) < 0)	
+			printf("Temperature < absolute zero for Fahrenheit!\n");
+		else
+			printf("%.2f C\n%.2f K\n", F_C(c), C_K(F_C(c)));
+		printf ("%.2f K:\n", c);
+		if (c < 0)
+			printf("Temperature < absolute zero for Kelvin!\n");
+		else
+			printf("%.2f C\n%.2f F\n", K_C(c), C_F(K_C(c)));
+		}
 
-	grad = atof(argv[1]);
-
-	if (argc == 2)
-	{
-		design = 'C';
-		change(grad, design);
-		printf("\n");
-
-		design = 'F';
-		change(grad, design);
-		printf("\n");
-
-		design = 'K';
-		change(grad, design);
-		printf("\n");
-	}
-	else
-	{
-		design = *argv[2];
-		change(grad, design);
-	}
-	return 0;
+return 0;
 }
- 
